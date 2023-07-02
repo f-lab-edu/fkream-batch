fc2ff7ee2e49a3ad466bd35807c207296e786dac
JooDukHan <bfinecpa1@naver.com>
2023-07-02 10:54:16 +0900

    안 쓰는 import 제거

--- src/main/java/com/flab/fkreambatch/job/statistics/MakeStatisticsOfDealJobConfig.java
+++ src/main/java/com/flab/fkreambatch/job/statistics/MakeStatisticsOfDealJobConfig.java
@@ -2,25 +2,15 @@ package com.flab.fkreambatch.job.statistics;
 
 
 import com.flab.fkreambatch.kafka.DealStatisticsService;
-import com.flab.fkreambatch.repository.DealStatisticsRepository;
-
-import javax.sql.DataSource;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.scope.context.ChunkContext;
-import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.SingleColumnRowMapper;
 
 @Slf4j
 @Configuration
