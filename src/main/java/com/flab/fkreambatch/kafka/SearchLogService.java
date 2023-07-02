071cb62a7a8d9515c00cbdcf88186234fc7915fe
JooDukHan <bfinecpa1@naver.com>
2023-07-02 10:24:01 +0900

    1시간 단위 검색 데이터 통계 작업 기능 추가

--- src/main/java/com/flab/fkreambatch/kafka/SearchLogService.java
+++ src/main/java/com/flab/fkreambatch/kafka/SearchLogService.java
@@ -1,15 +1,24 @@
 package com.flab.fkreambatch.kafka;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.flab.fkreambatch.elastic.SearchRepository;
+import com.flab.fkreambatch.entity.SearchDocument;
 import com.flab.fkreambatch.entity.SearchLog;
 import com.flab.fkreambatch.repository.SearchLogRepository;
+import java.time.Duration;
 import java.time.Instant;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.time.temporal.ChronoUnit;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.springframework.kafka.annotation.KafkaListener;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -17,20 +26,50 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class SearchLogService {
 
-    private final SearchLogRepository searchLogRepository;
+    private final SearchRepository searchRepository;
+    private final KafkaConsumerFactory consumerFactory;
 
-    @KafkaListener(topics = "${kafka.topic.search-log}"
-        , groupId = "${kafka.group-id}")
-    public void listenWithHeaders(ConsumerRecord<String, String> record)
-        throws JsonProcessingException {
-        String content = record.value();
-        LocalDateTime createdAt = ChangeTimestampToLocalDateTime(record.timestamp());
-        SearchLog searchLog = new SearchLog(content, createdAt);
-        searchLogRepository.insert(searchLog);
-    }
+    public void makeRankingOfRealTimeSearchTerms() {
+        KafkaConsumer<Object, Object> kafkaConsumer = consumerFactory.createDealStatisticsConsumer();
+        Map<String, Integer> countDataByTerms = new HashMap<>();
 
-    private LocalDateTime ChangeTimestampToLocalDateTime(Long timestamp) {
-        Instant instant = Instant.ofEpochMilli(timestamp);
-        return LocalDateTime.ofInstant(instant, ZoneId.systemDefault());
+        boolean isContinue = true;
+        try (kafkaConsumer) {
+            while (isContinue) {
+                ConsumerRecords<Object, Object> records = kafkaConsumer.poll(
+                    Duration.ofMillis(100));
+                if (records.isEmpty()) {
+                    break;
+                }
+                for (ConsumerRecord<Object, Object> record : records) {
+                    LocalDateTime createdAt = LocalDateTime.ofInstant(
+                        Instant.ofEpochMilli(record.timestamp()), ZoneId.systemDefault());
+                    if (LocalDateTime.now().truncatedTo(ChronoUnit.HOURS).isAfter(createdAt)) {
+                         String term = (String)record.value();
+                        if (countDataByTerms.containsKey(term)) {
+                            countDataByTerms.put(term, countDataByTerms.get(term)+1);
+                        } else {
+                            countDataByTerms.put(term, 1);
+                        }
+                    }
+                    else {
+                        isContinue = false;
+                        break;
+                    }
+                }
+            }
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            throw new RuntimeException(e);
+        }
+
+        // 4~5시 5시 5분까지 처리중이니까 5시 createdAt 으로 들어간다
+        for (Entry<String, Integer> entry : countDataByTerms.entrySet()) {
+            SearchDocument searchDocument = SearchDocument.builder()
+                .searchWord(entry.getKey())
+                .searchCount(entry.getValue())
+                .createdAt(LocalDateTime.now().truncatedTo(ChronoUnit.HOURS)).build();
+            searchRepository.save(searchDocument);
+        }
     }
 }
