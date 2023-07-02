82c9391216cff519afe535f63799a694877ed1fa
JooDukHan <bfinecpa1@naver.com>
2023-07-02 11:06:13 +0900

    검색 데이터 집계 서비스 테스트 코드 수정

--- src/test/java/com/flab/fkreambatch/kafka/SearchLogServiceTest.java
+++ src/test/java/com/flab/fkreambatch/kafka/SearchLogServiceTest.java
@@ -1,14 +1,30 @@
 package com.flab.fkreambatch.kafka;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.flab.fkreambatch.elastic.SearchRepository;
+import com.flab.fkreambatch.entity.SearchDocument;
 import com.flab.fkreambatch.entity.SearchLog;
 import com.flab.fkreambatch.repository.SearchLogRepository;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.temporal.ChronoUnit;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
@@ -22,33 +38,41 @@ import org.springframework.boot.test.context.SpringBootTest;
 class SearchLogServiceTest {
 
     @Mock
-    private SearchLogRepository searchLogRepository;
+    private SearchRepository searchRepository;
+
+    @Mock
+    private KafkaConsumerFactory consumerFactory;
 
     @InjectMocks
     private SearchLogService searchLogService;
 
-    @Captor
-    private ArgumentCaptor<SearchLog> searchLogCaptor;
-
-    public SearchLogServiceTest() {
+    @BeforeEach
+    void setUp() {
         MockitoAnnotations.openMocks(this);
+        searchLogService = new SearchLogService(searchRepository, consumerFactory);
     }
 
     @Test
-    public void testListenWithHeaders() throws JsonProcessingException {
-        String content = "search log content";
-        long timestamp = System.currentTimeMillis();
-        ConsumerRecord<String, String> record = new ConsumerRecord<>("topic", 0, 0L, "key",
-            content);
+    void makeRankingOfRealTimeSearchTerms_shouldSaveSearchDocuments() {
+        // Arrange
+        KafkaConsumer<Object, Object> kafkaConsumer = mock(KafkaConsumer.class);
+        when(consumerFactory.createDealStatisticsConsumer()).thenReturn(kafkaConsumer);
 
-        searchLogService.listenWithHeaders(record);
+        ConsumerRecords<Object, Object> records = mock(ConsumerRecords.class);
+        when(kafkaConsumer.poll(any())).thenReturn(records);
+        when(records.isEmpty()).thenReturn(false);
 
-        Mockito.verify(searchLogRepository).insert(searchLogCaptor.capture());
-        SearchLog capturedSearchLog = searchLogCaptor.getValue();
-        assertEquals(content, capturedSearchLog.getContent());
+        LocalDateTime currentHour = LocalDateTime.now().truncatedTo(ChronoUnit.HOURS);
+        LocalDateTime createdAt = currentHour.minusMinutes(30);
 
-        Instant instant = Instant.ofEpochMilli(timestamp);
-        LocalDateTime expectedCreatedAt = LocalDateTime.ofInstant(instant, ZoneId.systemDefault());
-        assertEquals(expectedCreatedAt, capturedSearchLog.getCreatedAt());
+        ConsumerRecord<Object, Object> record = mock(ConsumerRecord.class);
+        when(record.timestamp()).thenReturn(createdAt.toInstant(ZoneOffset.UTC).toEpochMilli());
+        when(record.value()).thenReturn("term1");
+
+        when(records.iterator()).thenReturn(Collections.singletonList(record).iterator());
+
+        searchLogService.makeRankingOfRealTimeSearchTerms();
+
+        verify(searchRepository, times(1)).save(any(SearchDocument.class));
     }
 }
